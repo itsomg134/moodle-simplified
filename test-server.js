17c7e608875346c0cd6ee0ff14492aeff20b7e66
Om Gedam <omgedam123098@gmail.com>
2025-08-29 21:53:42 +0530

    CODE FOR ALL ASSIGMENT BUT WORKING MODDLE ZIP

---
+++ test-server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ status: "OK", message: "Test server is working" });
+});
+
+app.get("/auth", (req, res) => {
+  res.json({ message: "Auth endpoint would be here" });
+});
+
+app.get("/callback", (req, res) => {
+  res.json({ message: "Callback endpoint would be here" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+  console.log("Test endpoint: http://localhost:" + PORT);
+});
